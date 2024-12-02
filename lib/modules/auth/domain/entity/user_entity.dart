b6946e45945a114280873baa06839eaad0dc8268
nabilgabr <nabiljbr490@gmail>
2024-12-02 09:34:12 +0200

    add user entity

---
+++ lib/modules/auth/domain/entity/user_entity.dart
@@ -0,0 +1,9 @@
+class UserEntity {
+  final String userEmail, userPassword, userName, userPhone;
+
+  UserEntity(
+      {required this.userEmail,
+      required this.userPassword,
+      required this.userName,
+      required this.userPhone});
+}
