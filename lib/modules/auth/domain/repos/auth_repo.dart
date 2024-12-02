7faf39edcaa9860074b2703df72e3a667de82c69
nabilgabr <nabiljbr490@gmail>
2024-12-02 09:49:19 +0200

    add auth repo file

---
+++ lib/modules/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:iteru_app/core/errors/failure.dart';
+import 'package:iteru_app/modules/auth/domain/entity/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      {required userEmail,
+      required userPassword,
+      required userName,
+      required userPhone});
+}
